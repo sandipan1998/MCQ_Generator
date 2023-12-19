694c7e145ab1d97efddd84671b458caa76e255e9
sandipan1998 <msandipan98@gmail.com>
2023-12-19 09:12:45 +0530

    Updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='Sandipan',
+    author='sandipan1998',
     author_email='msandipan98@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
